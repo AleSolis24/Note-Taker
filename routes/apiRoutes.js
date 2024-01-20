408ae558cc3805aec258457b1c1f2160702cfb61
AleSolis24 <aletitan24@icloud.com>
2024-01-20 18:49:18 -0500

    updated

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,15 +1,17 @@
+const path = require('path');
 const router = require('express').Router();
 const fs = require('fs/promises');
+
 const dbJSON = path.join(__dirname, 'db.json');
-const path = require('path');
+
 
 
 // get request for fetching the api notes route 
 router.get('/api/notes', async (req, res) => {
     try {
-        const u = await fs.readFile(dbJSON,'utf8');
-        const n = JSON.parse(u);
-        res.json(n); 
+        const data = await fs.readFile(dbJSON,'utf8');
+        const notes = JSON.parse(data);
+        res.json(notes); 
     } catch (err) {
         console.error('There a error with your notes!!', err);
         res.status(500).send('Server Error!!');
@@ -19,14 +21,14 @@ router.get('/api/notes', async (req, res) => {
 // post request for the user to add a new note in the request body. 
 router.post('/api/notes', async (req, res)=> {
     try {
-        const u = await fs.readFile(dbJSON, 'utf8');
+        const data = await fs.readFile(dbJSON, 'utf8');
         const userNewNotes = {
             id: Math.floor(math.random() * 1000),
             text: req.body.text,
             title: req.body.title
         };
        note.push(userNewNotes);
-       await fs.writeFile(dbJSON, JSON.stringify(u));
+       await fs.writeFile(dbJSON, JSON.stringify(data));
        res.json(userNewNotes);
     } catch (err) {
         console.error('There a Error!');
@@ -36,3 +38,4 @@ router.post('/api/notes', async (req, res)=> {
 })
 
 module.exports = router; 
+
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
-const apiRoutes = require('./routes/apiRoutes');
+const apiRoutes = require('./Develop/routes/apiRoutes');
+const htmlRoutes = require('./Develop/routes/htmlRoutes');
 
 
 const app = express();
@@ -9,5 +10,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.use('/api', apiRoutes);
+app.use('/', htmlRoutes); 
 
-app.listen(PORT, () => console.log(`Coneected to ${PORT}`));
+app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
