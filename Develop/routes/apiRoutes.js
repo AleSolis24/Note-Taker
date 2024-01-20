f78ac6ac8e3888ac7a0ed705f081326f62d2de7a
AleSolis24 <aletitan24@icloud.com>
2024-01-20 18:07:39 -0500

    added router post request

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -15,6 +15,23 @@ router.get('/api/notes', async (req, res) => {
     }
 })
 
-
+// post request for the user to add a new note in the request body. 
+router.post('/api/notes', async (req, res)=> {
+    try {
+        const u = await fs.readFile(dbJSON, 'utf8');
+        const userNewNotes = {
+            id: Math.floor(math.random() * 1000),
+            text: req.body.text,
+            title: req.body.title
+        };
+       note.push(userNewNotes);
+       await fs.writeFile(dbJSON, JSON.stringify(u));
+       res.json(userNewNotes);
+    } catch (err) {
+        console.error('There a Error!');
+        // if there is a error send the user a 500 error with a provided message for error
+        res.status(500).send('There a server error!!');
+    }
+})
 
 module.exports = router; 
---
+++ Develop/server.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const apiRoutes = require('./routes/apiRoutes');
+
+
+const app = express();
+const PORT = process.env.PORT || 3001
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.use('/api', apiRoutes);
+
+app.listen(PORT, () => console.log(`Coneected to ${PORT}`));
