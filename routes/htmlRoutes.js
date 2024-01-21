6a1cecad128558d8b6852b251636866441fd91c6
AleSolis24 <aletitan24@icloud.com>
2024-01-20 20:52:14 -0500

    adding notes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,10 +1,12 @@
 const path = require('path');
 const router = require('express').Router();
 
+// homepage get request that links to the index.html file 
 router.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
+// get request to work on the notes.html 
 router.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/notes.html'));
 });
--- server.js
+++ server.js
@@ -6,10 +6,12 @@ const htmlRoutes = require('./routes/htmlRoutes');
 const PORT = process.env.PORT || 3001
 const app = express();
 
+// Middleware express 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static('public'));
 app.use('/api', apiRoutes);
 app.use('/', htmlRoutes); 
 
+// this is letting me know my server is up 
 app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
